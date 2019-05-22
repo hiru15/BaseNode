ad94114eae4396f5af55bbe79015c0d7ad1ccd54
Felipe <felipe.benitez1401@alumnos.ubiobio.cl>
2019-05-22 09:28:12 -0400

    Nueva Ruta con middleware

--- controladores/usuarios.controlador.js
+++ controladores/usuarios.controlador.js
@@ -6,6 +6,11 @@ function holaMundo(req, res) {
     res.status(200).send({ token: token.crearToken(),  mensaje: "Hola Mundo" })
 }
 
+function ValidToken(req, res) {
+    res.status(200).send({ mensaje: "Token Valido Perro !" })
+}
+
 module.exports = {
-    holaMundo
+    holaMundo,
+    ValidToken
 }
--- rutas/usuarios.ruta.js
+++ rutas/usuarios.ruta.js
@@ -6,6 +6,9 @@ var controladorUsuarios = require('../controladores/usuarios.controlador.js')
 /* Se crean la variables para asignar rutas */
 var api = express.Router();
 
+var md_aut = require("../middleware/aut.js")
+
 api.get('/hello', controladorUsuarios.holaMundo)
+api.get('/token', md_aut.autenticacion, controladorUsuarios.holaMundo)
 
 module.exports = api; 
